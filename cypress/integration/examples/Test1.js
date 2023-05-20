148c546c6135d1fa18110c0211419809d4fbec7b
AENAO <aenaoclub@gmail.com>
2023-05-20 12:41:26 +0300

    first tests

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function()
 {
 
@@ -5,6 +7,9 @@ it('My First Test Case', function()
 {
 
 cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+cy.get('.search-keyword').type('ca')
+cy.wait(2000)
+cy.get('.product:visible').should('have.length', 4)
 
 })
 

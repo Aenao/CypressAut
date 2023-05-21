af2ba11b499618f6d3200e410b8ce6934884bec9
AENAO <aenaoclub@gmail.com>
2023-05-21 12:35:22 +0300

    add to cart

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -11,6 +11,10 @@ cy.get('.search-keyword').type('ca')
 cy.wait(2000)
 cy.get('.product:visible').should('have.length', 4)
 
+cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+
+
 })
 
 
